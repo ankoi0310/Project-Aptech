497ae25c54d8e616f838f3b91181c396135fdbc3
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-05-27 00:18:26 +0700

    add ajax method

--- NGO/NGO/wwwroot/js/site.js
+++ NGO/NGO/wwwroot/js/site.js
@@ -1,1 +1,76 @@
-﻿
+﻿removeSorting = () => {
+    $('th:last-child').removeAttr("class");
+}
+
+showInPopup = (url, title) => {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (res) {
+            removeSorting();
+            $("#form-modal .modal-body").html(res);
+            $("#form-modal .modal-title").html(title);
+            $("#form-modal").modal("show");
+        }
+    })
+}
+
+jQueryAjaxPost = (form) => {
+    //$('#editor').html() == "<p><br data-cke-filler=\"true\"></p>" ? $('#Content').val(null) : $('#Content').val($('#editor').html());
+    removeSorting();
+    try {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    $("#view-all").html(res.html);
+                    setInterval('location.reload()', 100);
+                    $("#form-modal").modal('hide');
+                    $("#form-modal .modal-body").html('');
+                    $("#form-modal .modal-title").html('');
+                    alert("Submitted Succesfully");
+                } else {
+                    $("#form-modal .modal-body").html(res.html);
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+
+    return false;
+}
+
+jQueryAjaxDelete = form => {
+    removeSorting();
+    if (confirm("Are you sure to delete this record?")) {
+        try {
+            $.ajax({
+                type: "POST",
+                url: form.action,
+                data: new FormData(form),
+                contentType: false,
+                processData: false,
+                success: function (res) {
+                    $("#view-all").html(res.html);
+                    setInterval('location.reload()', 100);
+                    $.notify("Deleted successfully", { globalPosition: "top center", className: "success" });
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return false;
+}
