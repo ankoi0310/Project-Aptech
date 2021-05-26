dd9202aeb13e58576553e4ca19a26db4fc67d2ae
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-05-27 00:21:02 +0700

    update Member class

--- NGO/NGO/Models/MappingClass/Member.cs
+++ NGO/NGO/Models/MappingClass/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,22 +14,32 @@ namespace NGO.Models.MappingClass
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("Id", TypeName = "int")]
         public int Id { get; set; }
+
         [Column("Name", TypeName = "nvarchar(50)")]
         public string Name { get; set; }
+
         [Column("Phone", TypeName = "nvarchar(50)")]
         public string Phone { get; set; }
+
         [Column("Email", TypeName = "nvarchar(50)")]
         public string Email { get; set; }
+
         [Column("BankAccount", TypeName = "nvarchar(50)")]
+        [DisplayName("Bank Account")]
         public string BankAccount { get; set; }
+
         [Column("BankName", TypeName = "nvarchar(50)")]
+        [DisplayName("Bank Name")]
         public string BankName { get; set; }
+
         [Column("Username", TypeName = "nvarchar(50)")]
         public string Username { get; set; }
+
         [Column("Password", TypeName = "nvarchar(50)")]
         public string Password { get; set; }
 
         [Column("MemberTypeId", TypeName = "int")]
+        [DisplayName("Member Type")]
         public int MemberTypeId { get; set; }
         [ForeignKey("MemberTypeId")]
         public MemberType MemberType { get; set; }
