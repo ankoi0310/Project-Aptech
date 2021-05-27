28fb3b90a93eafff97f6983bbe549551e97d9b72
quockhanh1702 <74188095+quockhanh1702@users.noreply.github.com>
2021-05-27 22:01:21 +0700

    UI day nhe mn

---
+++ NGO/NGO/Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGO.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Donation()
+        {
+            return View();
+        }
+
+        public IActionResult CauseDetail()
+        {
+            return View();
+        }
+
+        public IActionResult EventDetail()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
