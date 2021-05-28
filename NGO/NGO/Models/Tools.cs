956aee2efbaa624efc4ff0ee19a24c705f2bbb2f
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-05-29 00:06:16 +0700

    create Tools

---
+++ NGO/NGO/Models/Tools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGO.Models
+{
+    public static class Tools
+    {
+        static readonly NGOContext context = new();
+
+        // MemberType
+        public static string GetMemberType(int id) => context.MemberTypes.Find(id).Name;
+    }
+}
