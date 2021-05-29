66f35042410044bcc6b88d48835b018f3110f27f
unknown <duclq2810@gmail.com>
2021-05-29 19:24:33 +0700

    Update Program and ProgramDetail front-end

--- NGO/NGO/Controllers/MainController.cs
+++ NGO/NGO/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NGO.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ namespace NGO.Controllers
 {
     public class MainController : Controller
     {
+        private readonly NGOContext _context;
+
+        public MainController(NGOContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,8 +31,36 @@ namespace NGO.Controllers
             return View();
         }
 
-        public IActionResult CauseDetail()
+        public IActionResult ListProgram()
         {
+            var now = DateTime.Now;
+            var listPro = _context.Programs.ToList();
+            var listTopPro = _context.Programs.Where(x=> DateTime.Compare(x.BeginDate,now)<=0 && DateTime.Compare(x.EndDate, now)>=0)
+                .OrderByDescending(x => x.NeedAmount).ToList();
+           
+            if (listTopPro.Count >= 5)
+            {
+                listTopPro = listTopPro.Take(5).ToList();
+            }
+            var listImage = _context.ProgramImages.ToList();
+            var listDonationCate = _context.DonationCategories.ToList();
+            var listDonationRecord = _context.DonationRecords.ToList();
+            ViewBag.ListProgram = listPro;
+            ViewBag.ListTopProgram = listTopPro;
+            ViewBag.listImage = listImage;
+            ViewBag.ListDonationCate = listDonationCate;
+            ViewBag.ListDonationRecord = listDonationRecord;
+            return View();
+        }
+        public IActionResult ProgramDetail(int Id)
+        {
+            Models.MappingClass.Program pro = _context.Programs.Find(Id);
+            ViewBag.Program = pro;
+            var listDonationRecord = _context.DonationRecords.Where(x=>x.ProgramId == Id).ToList();
+            ViewBag.ListDonationRecord = listDonationRecord;
+            var listImage = _context.ProgramImages.ToList();
+            ViewBag.listImage = listImage;
+            var listDonationCate = _context.DonationCategories.ToList();
             return View();
         }
 
