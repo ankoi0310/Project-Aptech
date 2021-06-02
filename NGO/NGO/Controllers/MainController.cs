6b6dc096f9d3ca619c858baee88c2f97827122cf
huytht <huytht2001@gmail.com>
2021-06-02 22:22:21 +0700

    edit MainController

--- NGO/NGO/Controllers/MainController.cs
+++ NGO/NGO/Controllers/MainController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NGO.Models;
+using NGO.Models.MappingClass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +13,13 @@ namespace NGO.Controllers
 {
     public class MainController : Controller
     {
+        private readonly NGOContext _context;
+
+        public MainController()
+        {
+            _context ??= new NGOContext();
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,8 +35,72 @@ namespace NGO.Controllers
             return View();
         }
 
-        public IActionResult ProjectDetail()
+        public async Task<IActionResult> Update([Bind("Id,Name,Phone,Email,BankAccount,BankName,Username,Password,MemberTypeId,Active")] Member member)
         {
+            if (ModelState.IsValid)
+            {
+                Member oldMember = await _context.Members.FindAsync(member.Id);
+                oldMember.Name = member.Name ?? oldMember.Name; 
+                oldMember.Phone = member.Phone ?? oldMember.Phone; 
+                oldMember.Email = member.Email ?? oldMember.Email; 
+                oldMember.BankAccount = member.BankAccount ?? oldMember.BankAccount; 
+                oldMember.BankName = member.BankName ?? oldMember.BankName;
+                HttpContext.Session.SetString("MEMBER", JsonConvert.SerializeObject(oldMember));
+                _context.Update(oldMember);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("information");
+        }
+
+
+        public IActionResult ListProgram(int id)
+        {
+            var now = DateTime.Now;
+            List<NGO.Models.MappingClass.Program> listPro = new List<NGO.Models.MappingClass.Program>();
+            if (id == 0)
+            {
+                listPro = _context.Programs.ToList();
+            }
+            else
+            {
+                listPro = _context.Programs.Where(x => x.DonationCategoryId == id).ToList();
+            }
+
+            var listTopPro = _context.Programs.Where(x => DateTime.Compare(x.BeginDate, now) <= 0 && DateTime.Compare(x.EndDate, now) >= 0)
+                .OrderByDescending(x => x.NeedAmount).ToList();
+
+            if (listTopPro.Count >= 5)
+            {
+                listTopPro = listTopPro.Take(5).ToList();
+            }
+            var listImage = _context.ProgramImages.ToList();
+            var listDonationCate = _context.DonationCategories.ToList();
+            var listDonationRecord = _context.DonationRecords.ToList();
+            ViewBag.ListProgram = listPro;
+            ViewBag.ListTopProgram = listTopPro;
+            ViewBag.listImage = listImage;
+            ViewBag.ListDonationCate = listDonationCate;
+            ViewBag.ListDonationRecord = listDonationRecord;
+            return View();
+        }
+        public IActionResult ProgramDetail(int Id)
+        {
+            Models.MappingClass.Program pro = _context.Programs.Find(Id);
+            ViewBag.Program = pro;
+            var listDonationRecord = _context.DonationRecords.Where(x => x.ProgramId == Id).ToList();
+            ViewBag.ListDonationRecord = listDonationRecord;
+            var listImage = _context.ProgramImages.ToList();
+            ViewBag.listImage = listImage;
+            var listPayment = _context.PaymentTypes.ToList();
+            ViewBag.listPayment = listPayment;
+            var listDonationCate = _context.DonationCategories.ToList();
+
+            var memberString = HttpContext.Session.GetString("MEMBER");
+
+            if (memberString != null)
+            {
+                ViewBag.Member = JsonConvert.DeserializeObject<Member>(memberString);
+            }
             return View();
         }
 
@@ -32,24 +108,19 @@ namespace NGO.Controllers
         {
             return View();
         }
+
         public IActionResult Contact()
         {
             return View();
         }
+
         public IActionResult Information()
         {
-            return View();
-        }
-        public IActionResult ListProject()
-        {
-            return View();
-        }
-        public IActionResult Causes()
-        {
-            return View();
-        }
-        public IActionResult Transaction()
-        {
+            if (HttpContext.Session.GetString("MEMBER") != null)
+            {
+                Member member = JsonConvert.DeserializeObject<Member>(HttpContext.Session.GetString("MEMBER"));
+                return View(member);
+            }
             return View();
         }
     }
